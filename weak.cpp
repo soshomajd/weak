0ecfe189d14ed2df1c401697e4ab501499c7ffe9
sosho majd <46541607+soshomajd@users.noreply.github.com>
2019-01-15 12:54:56 +0330

    Add files via upload

---
+++ weak.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main()
+{
+int x;
+cout<<"enter num of waek: ";
+cin>>x;
+switch (x){
+	case 1:
+	cout<<"saturday";
+	break;
+	case 2 :
+	cout<<"sunday";
+	break;
+	case 3 :
+	cout<<"monday";
+	break;
+	case 4 :
+	cout<<"tuesday";
+	break;
+		case 5 :
+	cout<<"wednesday";
+	break;
+		case 6 :
+	cout<<"thursday";
+	break;
+		case 7 :
+	cout<<"friday";
+	break;
+}
+
+
+}
+
